b12e5f5c26ba7029760691d3eaecb6812af90ac0
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-30 14:39:58 -0400

    Add some results

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,18 +1,23 @@
 def merge_sort(arr)
   return arr if arr.length < 2
-  left = merge_sort(0...arr[arr.length/2])
+  left = merge_sort(arr[...arr.length/2])
   right = merge_sort(arr[arr.length/2...])
   return merge(left, right)
 end
 
 def merge(left, right)
   result = []
-  while left.length > 0 && right.length > 0 #check that this works? 
+  while left.length > 0 && right.length > 0
     if left[0] < right[0]
       result << left.shift
     else
       result << right.shift
     end
   end
-  result.concat(left).concat(right) #at least one will be empty
+  result.concat(left).concat(right) #in case one is not empty
 end
+
+pp merge_sort([1, 2, 3, 4]) # => [1, 2, 3, 4]
+pp merge_sort([5, 4, 3, 2, 1]) # => [1, 2, 3, 4, 5]
+pp merge_sort([1]) # => [1]
+pp merge_sort([]) # => []
