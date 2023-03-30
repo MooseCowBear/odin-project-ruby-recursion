99a062e60375c44aa0a563f5a9fe2dde63c03267
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-30 14:35:06 -0400

    Add recursive merge sort method

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,20 +1,18 @@
 def merge_sort(arr)
-
+  return arr if arr.length < 2
+  left = merge_sort(0...arr[arr.length/2])
+  right = merge_sort(arr[arr.length/2...])
+  return merge(left, right)
 end
 
 def merge(left, right)
   result = []
-  while left && right 
+  while left.length > 0 && right.length > 0 #check that this works? 
     if left[0] < right[0]
       result << left.shift
     else
       result << right.shift
     end
   end
-  if left 
-    result.concat(left)
-  elsif right
-    result.concat(right)
-  end
-  result
+  result.concat(left).concat(right) #at least one will be empty
 end
